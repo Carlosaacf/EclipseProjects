37ac7fc310b8f911cebccc482221b3fc706261a0
Carlosaacf <carlosaacf@gmail.com>
2021-08-18 05:43:28 -0300

    atualizacao for3

--- src/main/java/Estruturas/for2.java
+++ src/main/java/Estruturas/for2.java
@@ -1,5 +1,11 @@
 package Estruturas;
 
 public class for2 {
-
+public static void main(String[] args) {
+	
+	for (int contador = 0; contador<=100; contador +=20, contador-=10)
+	{
+		System.out.printf("Contador = %d\n", contador);
+	}
+}
 }
