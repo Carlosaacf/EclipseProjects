89a7169e93d669d5f6864c03d2f803c0b73b047c
Carlosaacf <carlosaacf@gmail.com>
2021-08-18 06:23:57 -0300

    DesafioFor

---
+++ src/main/java/Estruturas/DesafioFor.java
@@ -0,0 +1,13 @@
+package Estruturas;
+
+public class DesafioFor {
+	public static void main(String[] args) {
+
+	
+		for(String v = "#"; !v.equals("######"); v +="#")
+		{
+			System.out.println(v);
+			
+		}
+	}
+}
