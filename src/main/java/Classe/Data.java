596200f3f1c9a05a8c79f54ed4b93d367d4770b7
Carlosaacf <carlosaacf@gmail.com>
2021-10-07 05:23:42 -0300

    Exercicios Equals

--- src/main/java/Classe/Data.java
+++ src/main/java/Classe/Data.java
@@ -7,18 +7,17 @@ public class Data {
 	int ano;
 
 	Data() {
-	//	dia = 1;
-	//	mes = 1;
-	//	ano = 1970;
-		
+		// dia = 1;
+		// mes = 1;
+		// ano = 1970;
 		this(1, 1, 1970);
 	}
 
-	Data(int dia, int mes, int ano){
-	this.dia = dia;
-	this.mes = mes;
-	this.ano = ano;
-}
+	Data(int dia, int mes, int ano) {
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
+	}
 
 	String obterDataFormatada() {
 		return String.format("%d/%d/%d", this.dia, mes, ano);
